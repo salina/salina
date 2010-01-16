90f2a0ea22e6ad80abea825d4d9beb46a5ad6c18
Jason Ford <jason@jason-ford.com>
2010-01-16 17:46:25 -0600

    Changed reference from pybible to bible - the default "easy_install bible" should get everything set up to work with this syntax

--- content/models.py
+++ content/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from pybible.djangoforms import VerseField
+from bible.djangoforms import VerseField
 
 
 class Classifier(models.Model):
